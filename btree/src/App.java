b8f1cd76e797554174e0738622a43fd009c34813
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2020-11-10 14:48:05 -0800

    added randoms

--- btree/src/App.java
+++ btree/src/App.java
@@ -1,10 +1,11 @@
-
+import java.util.ArrayList;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
        // ArbolB BabyYoda = new randomTree();
-        ArbolB b = new ArbolB<>(5);
+        ArbolB b = new ArbolB<>(100);
         b.Insert(8);
         b.Insert(9);
         b.Insert(10);
@@ -12,6 +13,15 @@ public class App {
         b.Insert(15);
         b.Insert(20);
         b.Insert(17);
+        Random random = new Random();
+        ArrayList<Integer> randoms = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            int randomNumber = random.nextInt(100 - 0) + 0;
+            randoms.add(randomNumber );
+        }
+        for (Integer integer : randoms) {
+            b.Insert(integer);
+        }
         b.Show();
     }
 
