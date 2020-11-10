3f5257a4b4409ba0c6a0b063fd6edae64e1cffb8
Marcelino18 <54329358+Marcelino18@users.noreply.github.com>
2020-11-10 12:44:23 -0800

    añadido el remove, pero sigue habiendo el nullpointerexception en lineas 98, 86 y 353

--- ArbolB.java
+++ ArbolB.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Stack;
 public class ArbolB<T extends Comparable<T>> {
 
     private int T;
@@ -71,22 +72,23 @@ public class ArbolB<T extends Comparable<T>> {
       x.n = x.n + 1;
     }
 
-    public void Insert(T key) {
+    public void Insert(final T key) {
       Node<T> r = root;
       if (r.n == 2 * T - 1) {
-        Node<T> s = new Node<T>();
+        Node<T> s = new Node();
         root = s;
         s.leaf = false;
         s.n = 0;
         s.child[0] = r;
         Split(s, 0, r);
-        insertValue(s, key);
+        _Insert(s, key);
       } else {
-        insertValue(r, key);
+        _Insert(r, key);
       }
     }
-
-    private void insertValue(Node<T> x, T k) {
+  
+    // Insert the node
+    final private void _Insert(Node<T> x, T k) {
   
       if (x.leaf) {
         int i = 0;
@@ -97,7 +99,7 @@ public class ArbolB<T extends Comparable<T>> {
         x.n = x.n + 1;
       } else {
         int i = 0;
-        for (i = x.n - 1; i >= 0 && k.compareTo( x.key[i]) < 0; i--) {
+        for (i = x.n - 1; i >= 0 && k.compareTo(x.key[i]) < 0; i--) {
         }
         ;
         i++;
@@ -108,7 +110,7 @@ public class ArbolB<T extends Comparable<T>> {
             i++;
           }
         }
-        insertValue(x.child[i], k);
+        _Insert(x.child[i], k);
       }
   
     }
@@ -117,7 +119,223 @@ public class ArbolB<T extends Comparable<T>> {
       Show(root);
     }
   
-    // Display
+    private void Remove(Node<T> x, T key) {
+      int pos = x.Find(key);
+      if (pos != -1) {
+        if (x.leaf) {
+          int i = 0;
+          for (i = 0; i < x.n && x.key[i] != key; i++) {
+          }
+          ;
+          for (; i < x.n; i++) {
+            if (i != 2 * T - 2) {
+              x.key[i] = x.key[i + 1];
+            }
+          }
+          x.n--;
+          return;
+        }
+        if (!x.leaf) {
+  
+          Node<T> pred = x.child[pos];
+          T predKey;
+          if (pred.n >= T) {
+            for (;;) {
+              if (pred.leaf) {
+                System.out.println(pred.n);
+                predKey = pred.key[pred.n - 1];
+                break;
+              } else {
+                pred = pred.child[pred.n];
+              }
+            }
+            Remove(pred, predKey);
+            x.key[pos] = predKey;
+            return;
+          }
+  
+          Node<T> nextNode = x.child[pos + 1];
+          if (nextNode.n >= T) {
+            T nextKey = nextNode.key[0];
+            if (!nextNode.leaf) {
+              nextNode = nextNode.child[0];
+              for (;;) {
+                if (nextNode.leaf) {
+                  nextKey = nextNode.key[nextNode.n - 1];
+                  break;
+                } else {
+                  nextNode = nextNode.child[nextNode.n];
+                }
+              }
+            }
+            Remove(nextNode, nextKey);
+            x.key[pos] = nextKey;
+            return;
+          }
+  
+          int temp = pred.n + 1;
+          pred.key[pred.n++] = x.key[pos];
+          for (int i = 0, j = pred.n; i < nextNode.n; i++) {
+            pred.key[j++] = nextNode.key[i];
+            pred.n++;
+          }
+          for (int i = 0; i < nextNode.n + 1; i++) {
+            pred.child[temp++] = nextNode.child[i];
+          }
+  
+          x.child[pos] = pred;
+          for (int i = pos; i < x.n; i++) {
+            if (i != 2 * T - 2) {
+              x.key[i] = x.key[i + 1];
+            }
+          }
+          for (int i = pos + 1; i < x.n + 1; i++) {
+            if (i != 2 * T - 1) {
+              x.child[i] = x.child[i + 1];
+            }
+          }
+          x.n--;
+          if (x.n == 0) {
+            if (x == root) {
+              root = x.child[0];
+            }
+            x = x.child[0];
+          }
+          Remove(pred, key);
+          return;
+        }
+      } else {
+        for (pos = 0; pos < x.n; pos++) {
+          if (x.key[pos].compareTo(key) > 0) {
+            break;
+          }
+        }
+        Node<T> tmp = x.child[pos];
+        if (tmp.n >= T) {
+          Remove(tmp, key);
+          return;
+        }
+        if (true) {
+          Node<T> nb = null;
+          T devider;
+  
+          if (pos != x.n && x.child[pos + 1].n >= T) {
+            devider = x.key[pos];
+            nb = x.child[pos + 1];
+            x.key[pos] = nb.key[0];
+            tmp.key[tmp.n++] = devider;
+            tmp.child[tmp.n] = nb.child[0];
+            for (int i = 1; i < nb.n; i++) {
+              nb.key[i - 1] = nb.key[i];
+            }
+            for (int i = 1; i <= nb.n; i++) {
+              nb.child[i - 1] = nb.child[i];
+            }
+            nb.n--;
+            Remove(tmp, key);
+            return;
+          } else if (pos != 0 && x.child[pos - 1].n >= T) {
+  
+            devider = x.key[pos - 1];
+            nb = x.child[pos - 1];
+            x.key[pos - 1] = nb.key[nb.n - 1];
+            Node<T> child = nb.child[nb.n];
+            nb.n--;
+  
+            for (int i = tmp.n; i > 0; i--) {
+              tmp.key[i] = tmp.key[i - 1];
+            }
+            tmp.key[0] = devider;
+            for (int i = tmp.n + 1; i > 0; i--) {
+              tmp.child[i] = tmp.child[i - 1];
+            }
+            tmp.child[0] = child;
+            tmp.n++;
+            Remove(tmp, key);
+            return;
+          } else {
+            Node<T> lt = null;
+            Node<T> rt = null;
+            boolean last = false;
+            if (pos != x.n) {
+              devider = x.key[pos];
+              lt = x.child[pos];
+              rt = x.child[pos + 1];
+            } else {
+              devider = x.key[pos - 1];
+              rt = x.child[pos];
+              lt = x.child[pos - 1];
+              last = true;
+              pos--;
+            }
+            for (int i = pos; i < x.n - 1; i++) {
+              x.key[i] = x.key[i + 1];
+            }
+            for (int i = pos + 1; i < x.n; i++) {
+              x.child[i] = x.child[i + 1];
+            }
+            x.n--;
+            lt.key[lt.n++] = devider;
+  
+            for (int i = 0, j = lt.n; i < rt.n + 1; i++, j++) {
+              if (i < rt.n) {
+                lt.key[j] = rt.key[i];
+              }
+              lt.child[j] = rt.child[i];
+            }
+            lt.n += rt.n;
+            if (x.n == 0) {
+              if (x == root) {
+                root = x.child[0];
+              }
+              x = x.child[0];
+            }
+            Remove(lt, key);
+            return;
+          }
+        }
+      }
+    }
+  
+    public void Remove(T key) {
+      Node<T> x = Search(root, key);
+      if (x == null) {
+        return;
+      }
+      Remove(root, key);
+    }
+  
+    public void Task(T a, T b) {
+      Stack<T> st = new Stack<>();
+      FindKeys(a, b, root, st);
+      while (st.isEmpty() == false) {
+        this.Remove(root, st.pop());
+      }
+    }
+  
+    private void FindKeys(T a, T b, Node<T> x, Stack<T> st) {
+      int i = 0;
+      for (i = 0; i < x.n && x.key[i].compareTo(b) < 0; i++) {
+        if (x.key[i].compareTo(a) > 0) {
+          st.push(x.key[i]);
+        }
+      }
+      if (!x.leaf) {
+        for (int j = 0; j < i + 1; j++) {
+          FindKeys(a, b, x.child[j], st);
+        }
+      }
+    }
+  
+    public boolean Contain(T k) {
+      if (this.Search(root, k) != null) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    // Show the node
     private void Show(Node<T> x) {
       assert (x == null);
       for (int i = 0; i < x.n; i++) {
@@ -130,17 +348,8 @@ public class ArbolB<T extends Comparable<T>> {
       }
     }
   
-    // Check if present
-    public boolean Contain(T k) {
-      if (this.Search(root, k) != null) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
     public static void main(String[] args) {
-      ArbolB<Integer> b = new ArbolB<Integer>(3);
+      ArbolB b = new ArbolB(3);
       b.Insert(8);
       b.Insert(9);
       b.Insert(10);
@@ -151,12 +360,9 @@ public class ArbolB<T extends Comparable<T>> {
   
       b.Show();
   
-      if (b.Contain(12)) {
-        System.out.println("\nEncontrado");
-      } else {
-        System.out.println("\nNo Encontrado");
-      }
-      ;
+      b.Remove(10);
+      System.out.println();
+      b.Show();
     }
   }
   
